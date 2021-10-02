0e57be52deb7d78035ab38881da31a425ca69e13
a-naraikin <anna.naraikin@yandex.ru>
2021-10-02 15:06:59 +0300

    add functions sumNotDiscount, sumWitchDiscount, deleteItem and tests

--- src/app/__tests__/Cart.test.ts
+++ src/app/__tests__/Cart.test.ts
@@ -7,7 +7,29 @@ test('new card should be empty', () => {
   expect(cart.items).toBe(undefined);
 });
 
-test('should return a non-empty cart', () => {
+test('should return...', () => {
+  const movieOne = new Movie(1001, 'Мстители', 250, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137);
+  const movieTwo = new Movie(1002, 'Мстители: Финал', 350, 2019, 'США', 'Avenge the fallen', ['фантастика', 'боевик', 'драма', 'приключения'], 181);
+
+  const cart = new Cart();
+  cart.add(movieOne);
+  cart.add(movieTwo);
+  
+  expect(cart.sumNotDiscount()).toBe(600);
+});
+
+test('should return...', () => {
+  const movieOne = new Movie(1001, 'Мстители', 250, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137);
+  const movieTwo = new Movie(1002, 'Мстители: Финал', 350, 2019, 'США', 'Avenge the fallen', ['фантастика', 'боевик', 'драма', 'приключения'], 181);
+
+  const cart = new Cart();
+  cart.add(movieOne);
+  cart.add(movieTwo);
+  
+  expect(cart.sumWitchDiscount(25)).toBe(150);
+});
+
+test('should return...', () => {
   const movieOne = new Movie(1001, 'Мстители', 250, 2012, 'США', 'Avengers Assemble!', ['фантастика', 'боевик', 'фэнтези', 'приключения'], 137);
   const movieTwo = new Movie(1002, 'Мстители: Финал', 350, 2019, 'США', 'Avenge the fallen', ['фантастика', 'боевик', 'драма', 'приключения'], 181);
 
@@ -15,5 +37,7 @@ test('should return a non-empty cart', () => {
   cart.add(movieOne);
   cart.add(movieTwo);
 
-  expect(cart.getAll().length).toBe(2);
+  cart.deleteItem(1001);
+  
+  expect(cart.getAll().length).toBe(1);
 });
--- src/app/server/Cart.ts
+++ src/app/server/Cart.ts
@@ -4,11 +4,26 @@ export default class Cart {
   private _items: Buyable[] = [];
   items: any;
 
-  add (item: Buyable): void {
+  add(item: Buyable): void {
     this._items.push(item);
   }
-  
+
   getAll(): Buyable[] {
     return [...this._items];
+  }
+
+  sumNotDiscount(): number {
+    const arr = this.getAll();
+    return arr.reduce((a, b) => a + b.price, 0);
+  }
+
+  sumWitchDiscount(discount: number): number {
+    const sumNotDiscount = this.sumNotDiscount();
+    return sumNotDiscount / 100 * discount;
+  }
+
+  deleteItem(id: number): void {
+    const elemById = this._items.findIndex(elem => elem.id === id);
+    this._items.splice(elemById, 1);
   }
 }
